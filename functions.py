83bc2d25f7052ec2c9ab1a90f60fca2733b74978
e-c-robert <128013374+e-c-robert@users.noreply.github.com>
2023-03-30 21:40:38 -0400

    everything is now a function
    readability is now sm better

--- functions.py
+++ functions.py
@@ -4,17 +4,53 @@ import guiWindow as g
 import calculations as c
 import tkinter as tk
 
-#finds the computers machine epsilon so we can actually get a 0 answer 
-#found this out the hard way 
-def fuck():
-    ep = 1
-    while True :
-        if ep+1 <=1:
-            ep *= 2
-            break
-        else:
-            ep /= 2
-    return ep 
+
+#repalces 'find' with found answer from before
+def verify(keyi, ans1):
+    for inde, valu in enumerate(keyi):
+        if valu == 'find': 
+            keyi[inde]=ans1
+    return keyi
+
+#label placement         
+def inwindow(fcount,answer):
+    #different f count so the display does not overlap  for next unknown
+    if fcount == 2:
+        answer.grid(row = 16, column = 1, sticky = 'w')
+    elif fcount == 1:
+        answer.grid(row = 16, column = 0, sticky = 'w')
+            
+#answer 
+def an(fcount, inputs1, strallvar1, strrequires1, requires1, functions1, ans3):
+    #for loop : goes through the inputs once
+    for ind1, value1, in enumerate(inputs1):
+        #if there is a variable to find 
+        if value1 == 'find':
+            #find key from dictionary and all the alt equations 
+            key1 = strrequires1[strallvar1[ind1]]#loop through alternate equations 
+            for alt in range(len(key1)):
+                #count for found variables 
+                count1 = 0
+                #loops through the inputs again to check if needed variables are filled 
+                for i,v in enumerate(inputs1):
+                    #if the varible is in the nth (alt) equation and it is not unknown and it is not empty 
+                    if strallvar1[i] in key1[alt] and v != '' and v!= 'find':
+                        count1 += 1 #count plus one to keep track of what we have and what we dont have 
+                    if count1 == key1[alt][-1]: #if we have the correct number of variables inputted 
+                        #key2 = variable version of key1
+                        key2 = requires1[strallvar1[ind1]][alt]
+                        #this is if an unknown was solved before as the keys dont change w the input list
+                        if 'find' in key2:
+                            key2 = verify(key2, ans3)
+                        #inputs key into correct function and zero() checks to see if the answer is less than epsilon
+                        ans3 = c.zero(functions1[ind1](key2))    
+                        #chnages input list so the for loops dont have a time 
+                        inputs1[ind1] = ans3
+                        #answer display 
+                        answer = tk.Label(g.window, text=strallvar1[ind1] + ': ' + str(ans3), fg = 'blue')
+                        inwindow(fcount,answer)
+                        fcount-=1
+                        return ans3
 
 #main function 
 def check():
@@ -32,26 +68,7 @@ def check():
 
     #list of all variables and their string form so that i cna call the correct index in places 
     allvar = [vi, vf, theta, xi, xf, yi, yf, t, fw, m]
-    strallvar = ['vi', 'vf','theta','xi','xf','yi','yf','t','fw','m']
-
-    #emplty list of inputs to be added in the next for loop 
-    inputs = []
-    
-    #find count for print statments later 
-    fcount = 0
-    
-    #adds to the input list 
-    for index, val in enumerate(allvar):
-        if val == 'find':
-            inputs.append(val)
-            fcount+=1
-        elif val != '':
-            inputs.append(allvar[index])
-        else :
-            inputs.append('')       
-
-    #all dictionaries and lists are in the same order (so they have the same index)
-    #even tho dictionaries are unorder, this was done for the sake of those coding 
+    strallvar = ['vi','vf','theta','xi','xf','yi','yf','t','fw','m']
 
     #string version of all the required variables and the number of required inputs 
     #dictionary -> keys: the missing variable ('find') : list of all alternative equations
@@ -67,6 +84,8 @@ def check():
                          ['vi','theta','yf','yi', 4]],
                    'fw':[['xf','xi','vi','theta','t','m', 6]],
                    'm':[['fw','t','xf','xi','vi','theta',6]]}
+
+
     #variable version of previous dictionary 
     #created so i can unpack the list
     #instead of a count, there is a unique marker for that alt equation so that
@@ -83,94 +102,29 @@ def check():
                       [vi,theta,yf,yi, 'y']],
                 'fw':[[xf,xi,vi,theta,t,m]],
                 'm':[[fw,t,xf,xi,vi,theta]]}
+
     #functions, alternative equations for each variable will be found and done within 
     #the function 
-    functions = ['', 
-                 '', 
-                 '', 
-                 c.m_xi, 
-                 c.m_xf,
-                 c.m_yi,
-                 c.m_yf, 
-                 c.m_t,
-                 c.m_fw,
-                 c.m_m]
+    functions = ['', '','', c.m_xi,c.m_xf, c.m_yi, c.m_yf, c.m_t,c.m_fw, c.m_m]
     
-    #for loop : goes through the inputs once
-    for ind1, value1, in enumerate(inputs):
-        #if there is a variable to find 
-        if value1 == 'find':
-            #find key from dictionary and all the alt equations 
-            key1 = strrequires[strallvar[ind1]]
-            #loop through alt equations 
-            for alt in range(len(key1)):
-                #count for found variables 
-                count1 = 0
-                #loops through the inputs again to check if needed variable are filled 
-                for i,v in enumerate(inputs):
-                    #if the varible is in the nth (alt) equation and it is not unknown and it is not empty 
-                    if strallvar[i] in key1[alt] and v != '' and v!='find':
-                        count1 += 1
-                    #if there are 2 variables to solve for then this is moved off the to side 
-                    if fcount == 2 and count1 == key1[alt][-1]:
-                        #calculates the answer 
-                        ans = functions[ind1](requires[strallvar[ind1]][alt])
-                        #if the found answer is less than the machine epsilon, ans is 0, yay
-                        if ans <= fuck(): 
-                            ans = 0
-                        #puts the answer in the input list for reference for next unknown 
-                        inputs[ind1] = ans
-                        #displays the ans in the gui 
-                        answer = tk.Label(g.window, text=strallvar[ind1] + ': ' + str(ans), 
-                                          fg = 'blue')
-                        answer.grid(row = 16, column = 1, sticky = 'w')
-                        #changes f count so the display does not overlap  for next unknown
-                        fcount = 1
-                    elif fcount == 1 and count1==key1[alt][-1]:
-                        #find the keys for the variables
-                        #because this would be on the second theoretical run 
-                        #there needs to be an if statement if this unknwon is dependent on the rpevious 
-                        #because im (emma) stupid and the calculation functions dont pull directly from the 
-                        #inputs list, the if statement is needed
-                        key2 = requires[strallvar[ind1]][alt]
-                        #loops through the values of the needed variables
-                        for inde, valu in enumerate(key2):
-                            if valu == 'find': 
-                                #changes the previously unknown to the found ans 
-                                key2[inde]=ans
-                        #now that the key is changed to the correct ans, we can overwrite that 
-                        #variable with this answer yay!
-                        ans = functions[ind1](key2)
-                        if ans <= fuck(): 
-                            ans = 0
-                        inputs[ind1] = ans
-                        answer = tk.Label(g.window, text=strallvar[ind1] + ': ' + str(ans), 
-                                          fg = 'blue')
-                        answer.grid(row = 16, column = 0, sticky = 'w')
-           
-    #bc im (emma) lazy and dont want to figure out a different way...
-    #this checks to see if there is still an unknown 
-    #this unknown would have been in the inputs list before the previously solved one
-    #BUT to solve this, the previously solved one had to be done first so rip 
-    #bitch is j copied pasted from above 
-    if 'find' in inputs :
-        for ind, value, in enumerate(inputs):
-            if value == 'find':
-                key1 = strrequires[strallvar[ind]]
-                for alt in range(len(key1)):
-                    count1 = 0
-                    for i,v in enumerate(inputs):
-                        if strallvar[i] in key1[alt] and v != '' and v!='find':
-                            count1 += 1
-                        if count1 == key1[alt][-1]: 
-                            key2 = requires[strallvar[ind]][alt]
-                            for inde, valu in enumerate(key2):
-                                if valu == 'find': 
-                                    key2[inde]=ans
-                            ans = functions[ind](key2)
-                            if ans <= fuck(): 
-                                ans = 0
-                            inputs[ind] = ans
-                            answer = tk.Label(g.window, text=strallvar[ind] + ': ' + str(ans), 
-                                              fg = 'blue')
-                            answer.grid(row = 16, column = 0, sticky = 'w')
+    #emplty list of inputs to be added in the next for loop 
+    inputs = []
+    
+    #find count for print statments later 
+    fcount = 0
+    
+    #adds to the input list 
+    for index, val in enumerate(allvar):
+        if val == 'find':
+            inputs.append(val)
+            fcount+=1
+        elif val != '':
+            inputs.append(allvar[index])
+        else :
+            inputs.append('')     
+    
+    #empty ans variable so it can be an imput for the functions
+    ans = ''
+    ans = an(fcount, inputs, strallvar, strrequires, requires, functions, ans)
+    if 'find' in inputs : # still?!?!?!??!?!
+       ans = an(1, inputs, strallvar, strrequires, requires, functions, ans)
